217406308ec05c350868eeb85230d8b8bbdffb4d
Yash Raj <yashraj941015@gmail.com>
2024-01-29 23:48:51 +0530

    Search 2D array II

---
+++ BinarySearch/Search2DArrayII.java
@@ -0,0 +1,25 @@
+
+
+package BinarySearch;
+
+public class Search2DArrayII {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int row = 0, col = matrix[0].length-1;
+
+       while(row<=matrix.length-1 && col>=0 ){
+           if(matrix[row][col]==target){
+               return true;
+           } else if(matrix[row][col]<target) {
+               row++;
+           } else {
+               col--;
+           }
+       }
+       return false;
+    }
+
+    // call the function for any value 
+    public static void main(String[] args) {
+        
+    }
+}
